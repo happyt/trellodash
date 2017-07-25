886adc13e434237b14722b0c5e9f6f15d8a84719
Ian Morton <ian.morton@sky.uk>
2017-07-25 14:50:09 +0100

    No config public

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // target viz configuration
-    trello: {
-        key: '64532d61a0717998dd9b66dfb84beb21',
-        token: '2fe820b16904c26222b1719cc69f5adb42f0030740ff8f8f35207b1791f589f0'
-    }
-};
